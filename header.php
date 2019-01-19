201e23160892ad7e90f6e252dab917fedc697ee8
Mona Lisa <had17007@byui.edu>
2019-01-18 23:13:29 -0700

    added construction page and fixed header links

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
     <div>
         <nav class="navbar navbar-expand-sm bg-info navbar-dark">
-            <a class="navbar-brand" href="index.html">Cory's Portfolio</a>
+            <a class="navbar-brand" href="index.php">Cory's Portfolio</a>
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="project.html">Project Page</a>
+                    <a class="nav-link" href="project.php">Project Page</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.html">Current Project</a>
+                    <a class="nav-link" href="index.php">Current Project</a>
                 </li>
             </ul>
         </nav>
