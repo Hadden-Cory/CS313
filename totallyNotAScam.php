38bf0b222306b046c6a59c6f98044a0eb3511303
Mona Lisa <had17007@byui.edu>
2019-01-26 15:31:30 -0700

    ...

--- totallyNotAScam.php
+++ totallyNotAScam.php
@@ -9,7 +9,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="plants.js"></script>
     <script src="scam.js"></script>
-</head>
+</head> 
 <body>
 <?php include("header.php"); ?>
 <Section>
