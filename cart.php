bd6352f9d6e8255c243c1a685ec2cbe2e88e25bd
Mona Lisa <had17007@byui.edu>
2019-01-26 23:44:54 -0700

    ...

--- cart.php
+++ cart.php
@@ -37,7 +37,6 @@ session_start();
         print($sessionData);
         echo $sessionData . "<select name='" . $name . "Removal' class='form-control'>";
         for ($i = 0; $i < $sessionData; $i++) {
-            $sessionData = 0;
             echo "<option value='" . $i . "'>" . $i . "</option>";
         }
     }
