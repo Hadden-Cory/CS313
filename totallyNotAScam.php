ec1339ef5b18f72cf274c7a06e65ffd93a591ed5
Mona Lisa <had17007@byui.edu>
2019-01-26 17:06:59 -0700

    ...

--- totallyNotAScam.php
+++ totallyNotAScam.php
@@ -21,7 +21,7 @@
 </div>
 <div class="row">
 <form>
-<a href=""><div class="col-sm-4 h-50 w-25 itemCard">
+<a href=""><div class="col-sm-4 h-50 itemCard">
 <h3>Gummy Bear Tree Seeds</h3> 
 <img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
 </div></a>
