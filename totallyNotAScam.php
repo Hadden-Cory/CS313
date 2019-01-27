b0f63701c73f27a2c8e09cbcf5e087ba5cbe0102
Mona Lisa <had17007@byui.edu>
2019-01-26 17:09:10 -0700

    ....

--- totallyNotAScam.php
+++ totallyNotAScam.php
@@ -1,40 +1,49 @@
 <html>
-<head>   
+
+<head>
     <title>TotallyNotAScam.com</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-     <link rel="stylesheet" type="text/css" href="plants.css">
-     <link rel="stylesheet" type="text/css" href="scam.css">
+    <link rel="stylesheet" type="text/css" href="plants.css">
+    <link rel="stylesheet" type="text/css" href="scam.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="plants.js"></script>
     <script src="scam.js"></script>
-</head>  
-<body> 
-<?php include("header.php");?>
-<Section>
-<div class="row h-25">
-  <div class="col-md-12">
-    <h1>Welcome to Totally Not A Scam Internet Wonders</h1>
-    <p>We have many amazing wonderments which you like very much, I think. You not get this anywhere else on internet or in shops or in grocery stores. We only have. Everything is half off for you, first customer.</p>
-  </div>
-</div>
-<div class="row">
-<form>
-<a href=""><div class="col-sm-4 h-50 itemCard">
-<h3>Gummy Bear Tree Seeds</h3> 
-<img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
-</div></a>
-<a href=""><div class="col-sm-4 h-50 w-25 itemCard">
-<h3>Gummy Bear Tree Seeds</h3> 
-<img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
-</div></a>
-<a href=""><div class="col-sm-4 h-50 w-25 itemCard">
-<h3>Gummy Bear Tree Seeds</h3> 
-<img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
-</div></a>
-</form>
-</div>
-</Section>
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+    <Section>
+        <div class="row h-25">
+            <div class="col-lg-12">
+                <h1>Welcome to Totally Not A Scam Internet Wonders</h1>
+                <p>We have many amazing wonderments which you like very much, I think. You not get this anywhere else on
+                    internet or in shops or in grocery stores. We only have. Everything is half off for you, first
+                    customer.</p>
+            </div>
+        </div>
+        <div class="row">
+                <a href="">
+                    <div class="col-sm-3 h-50 itemCard">
+                        <h3>Gummy Bear Tree Seeds</h3>
+                        <img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
+                    </div>
+                </a>
+                <a href="">
+                    <div class="col-sm-3 h-50  itemCard">
+                        <h3>Gummy Bear Tree Seeds</h3>
+                        <img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
+                    </div>
+                </a>
+                <a href="">
+                    <div class="col-sm-3 h-50 itemCard">
+                        <h3>Gummy Bear Tree Seeds</h3>
+                        <img src="/week03ponder/GummyBear.jpg" alt="Gummy Bears">
+                    </div>
+                </a>
+        </div>
+    </Section>
 </body>
+
 </html>
