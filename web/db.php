de5849f4108ea473bb5b287de43efc7977ac475b
Mona Lisa <had17007@byui.edu>
2019-02-05 22:36:41 -0700

    Testing db

--- web/db.php
+++ web/db.php
@@ -21,4 +21,11 @@ catch (PDOException $ex)
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
 ?>
