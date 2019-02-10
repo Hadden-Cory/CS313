7306ad8e26c4ce7a8916149a0d57aa991cd45d05
Mona Lisa <had17007@byui.edu>
2019-02-09 20:36:47 -0700

    ...

--- web/thereNBack.php
+++ web/thereNBack.php
@@ -46,7 +46,7 @@ catch (PDOException $ex)
 //   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
 // }
 
-echo '<table><tr><td>Shippers</td><td>Pickup As Early As</td></tr>';
+echo '<table><tr><td>Shippers</td><td>Pickup As Early As</td><td>Pickup City</td><td>Pickup State</td></tr>';
 foreach ($db->query('SELECT
 shipper.shipper_name, 
 shipment.shipment_start_date,
@@ -63,12 +63,15 @@ shipment.id_shipment DESC,
 ship_loc_city DESC,
 ship_loc_is_pickup ASC;') as $row)
 {
-    if ($row['ship_loc_is_pickup']){
     echo '<tr><td>'. $row['shipper_name'].'</td>';
+    
+    
+    if ($row['ship_loc_is_pickup']){
     echo '<td>'. $row['shipment_start_date'].'</td>';
     echo '<td>'. $row['ship_loc_state'].'</td>';
-    echo '<td>'. $row['ship_loc_city'].'</td></tr>';
+    echo '<td>'. $row['ship_loc_city'].'</td>';
     }
+    echo '</tr>';
 }
 echo '</table>';
 
