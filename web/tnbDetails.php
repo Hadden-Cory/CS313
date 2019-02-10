420d181f2259d162981cc19ef911a1449f7d2a9b
Mona Lisa <had17007@byui.edu>
2019-02-09 23:35:05 -0700

    ...

--- web/tnbDetails.php
+++ web/tnbDetails.php
@@ -16,11 +16,84 @@ session_start();
 
 <body>
 
-    <?php include("header.php");
+<?php include("header.php"); 
 
-    echo'<div id="container" class="r>
+echo '<div id="container" class="r>
+<div class="row h-50">
+<div class="col-sm-2 col-lg-2">
+</div>
+<div class="col-sm-8 col-lg-8">';
+
+try
+{
+    
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+echo'<div id="container" class="r>
 <div class="row h-50">
 <div class="col-sm-2 col-lg-2">';
+
+$statement = $db->query('
+SELECT
+    ship.shipment_start_date,
+    pfrom.pickup_from_city,
+    pfrom.pickup_from_state, 
+    ship.shipment_end_date,
+    shipto.ship_to_city,
+    shipto.ship_to_state
+from
+    shipment as ship
+    inner join shipper as shipr on ship.Shipper_id_shipper = shipr.id_shipper
+    inner join ship_to as shipto on ship.id_shipment = shipto.shipment_id_shipment 
+    inner join pickup_from as pfrom on ship.id_shipment = pfrom.shipment_id_shipment 
+where
+    id_shipment = 1;
+');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($db->query(
+    'SELECT
+    i.item_name,
+    i.item_description,
+    i.item_spcl_instructs,
+    i.shipment_id_shipment,
+    s.size_weight,
+    s.size_width,
+    s.size_depth,
+    s.size_height
+    from 
+    item as I
+    inner join size as s on i.size_id_size = s.id_size
+    where i.shipment_id_shipment = 1; 
+order by
+    s.size_weight DESC;') as $row)
+{
+    echo '<tr><td>'.$row['item_name'].'</td>';
+    echo '<td>'.$row['item_description'].'</td>';
+    echo '<td>'.$row['item_spcl_instructs'].'</td>';
+    echo '<td>'.$row['size_weight'].'</td>';
+    echo '<td>'.$row['size_depth'].'</td>';
+    echo '<td>'.$row['size_height'].'</td>';
+    echo '</tr>';
+}
 echo '<p>'.$_SESSION['selection'].'</p>';
 ?>
     </div>
