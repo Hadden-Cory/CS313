bf6eb8266927624f537e56d52c5e27c7c8044b5a
Mona Lisa <had17007@byui.edu>
2019-02-09 20:10:32 -0700

    ...

--- web/thereNBack.php
+++ web/thereNBack.php
@@ -34,17 +34,17 @@ catch (PDOException $ex)
 }
 
 
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
-{
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
+// foreach ($db->query('SELECT username, password FROM note_user') as $row)
+// {
+//   echo 'user: ' . $row['username'];
+//   echo ' password: ' . $row['password'];
+//   echo '<br/>';
 
-  $statement = $db->query('SELECT username, password FROM note_user');
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
-}
+//   $statement = $db->query('SELECT username, password FROM note_user');
+// while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+// {
+//   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+// }
 
 echo '<table>';
 foreach ($db->query('SELECT
