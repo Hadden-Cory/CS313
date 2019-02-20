7ab2aed84c1518a7280dd5cd30ab6ec943085d71
Mona Lisa <had17007@byui.edu>
2019-02-20 11:06:38 -0700

    ...

--- web/welcome.php
+++ web/welcome.php
@@ -5,11 +5,11 @@
         session_start();
         $id_shipper = isset($_SESSION['id_shipper']) ? intval($_SESSION['id_shipper']) : 0;
 
-        if ($id_shipper == 0 || $_SERVER["REQUEST_METHOD"] == "POST") {
+        /*if ($id_shipper == 0 || $_SERVER["REQUEST_METHOD"] == "POST") {
             session_destroy();
             header("Location: signIn.php");
             die();
-        }
+        }*/
 
         $shipper_query = $db->prepare('SELECT shipper_name FROM shipper WHERE id_shipper = :id_shipper;');
         $shipper_query->execute(array('id_shipper' => $id_shipper));
