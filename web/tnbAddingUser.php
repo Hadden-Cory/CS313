63ccbc7220793b038bebebec7f7d5f7dc69d1727
Mona Lisa <had17007@byui.edu>
2019-02-22 23:08:28 -0700

    Refactoring Isert user

--- web/tnbAddingUser.php
+++ web/tnbAddingUser.php
@@ -1,11 +1,11 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <html>
 
 <head>
-    <title>ThereNBack.com-Sign In</title>
+    <title>ThereNBack.com-Sign UP</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="plants.css">
     <link rel="stylesheet" type="text/css" href="tnb.css">
@@ -19,18 +19,13 @@
     <?php
     include("header.php");
     ?>
-    <form action="tnbVerifyUser.php" method="POST">
+    <form action="tnbUserInsert.php" method="POST">
         <label for="shipper-name">Username</label>
         <input type="text" name="shipper_name">
         <label for="shipper-password">Password</label>
         <input type="password" name="shipper_password">
         <input type="submit" name="submit" value="Submit">
-        <?php
-        if ($_SESSION['lastAttemptFailed']) {
-            echo "<p id='signInError' class='text-danger'>Login Failed: password or username were incorrect.</p>";
-            } ?>
-       
     </form>
-    <p>Not a member? Sign up <a href="tnbAddingUser.php">here</a></p>
 </body>
-</html>
+
+</html> 
