6021944ddbf68a59725ce4b588b9c61b3be09749
Mona Lisa <had17007@byui.edu>
2019-02-23 02:32:13 -0700

    ...

--- web/tnbShipmentInsert.php
+++ web/tnbShipmentInsert.php
@@ -12,7 +12,7 @@
     $DropOffState =$_POST["DropOffState"];
     $DropOffCity =$_POST["DropOffCity"];
 
-    $statment = $db->query('SELECT id_shipper FROM shipper WHERE shipper_name = \'$username\';');
+    $statement = $db->query('SELECT id_shipper FROM shipper WHERE shipper_name = \'$username\';');
     $statement->execute();
     $shipperId = $statment->fetch(PDO::FETCH_ASSOC);
     echo "<h1>FETCHED: ".$shipperId."</h1>";
