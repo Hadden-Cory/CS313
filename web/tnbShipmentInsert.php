6fdca0c2ed64f5b1843fdbc234ff57fc4998ae80
Mona Lisa <had17007@byui.edu>
2019-02-23 03:07:11 -0700

    ....

--- web/tnbShipmentInsert.php
+++ web/tnbShipmentInsert.php
@@ -1,37 +1,39 @@
-
 <?php
-     session_start();
-     require "dbConnect.php";
-     $db = get_db();
+session_start();
+require "dbConnect.php";
+$db = get_db();
 
-    $username = $_SESSION["username"];
-    $pickUpDate = $_POST["pickUpDate"];
-    $pickUpState =$_POST["pickUpState"];
-    $pickUpCity =$_POST["pickUpCity"];
-    $dropOffDate =$_POST["dropOffDate"];
-    $dropOffState =$_POST["dropOffState"];
-    $dropOffCity =$_POST["dropOffCity"];
-    $shipperId = 0;
+$username = $_SESSION["username"];
+$pickUpDate = $_POST["pickUpDate"];
+$pickUpState = $_POST["pickUpState"];
+$pickUpCity = $_POST["pickUpCity"];
+$dropOffDate = $_POST["dropOffDate"];
+$dropOffState = $_POST["dropOffState"];
+$dropOffCity = $_POST["dropOffCity"];
+$shipperId = 0;
 
 
-    foreach ($db->query("SELECT id_shipper FROM shipper WHERE shipper_name = '$username'") as $row) 
-        {
-            $shipperId = $row['id_shipper'];
-        }
+foreach ($db->query("SELECT id_shipper FROM shipper WHERE shipper_name = '$username'") as $row) {
+    $shipperId = $row['id_shipper'];
+}
 
-    $statement=$db->prepare("INSERT INTO shipment (shipper_id_shipper, shipment_start_date, shipment_end_date)
-                                VALUES(:shipperIdShipper, :shipmentStartDate, :shipmentEndDate)");
+if ($shipperId != 0) {
+    $statement = $db->prepare("INSERT INTO shipment (shipper_id_shipper, shipment_start_date, shipment_end_date)
+                                VALUES(:shipperIdShipper, :shipmentStartDate, :shipmentEndDate);");
     $statement->bindValue(":shipperIdShipper", $shipperId, PDO::PARAM_STR);
     $statement->bindValue(":shipmentStartDate", $pickUpDate, PDO::PARAM_STR);
     $statement->bindValue(":shipmentEndDate", $dropOffDate, PDO::PARAM_STR);
     $statement->execute();
+}
 
-   
+
+
+
 //    $statement = $db->prepare("INSERT INTO shipment (shipper_name, shipper_password_hash)
 //                                VALUES (:cleanUsername, :cleanPasswordHash)");
 //     $statement->bindValue(":cleanUsername", $username, PDO::PARAM_STR);
 //     $statement->execute();
 
-     header("Location: thereNBack.php");
-     exit;
-?>
+header("Location: thereNBack.php");
+exit;
+ 
