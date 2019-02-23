ecef6344f7e4bc7ab322de15b1e4985664c67b73
Mona Lisa <had17007@byui.edu>
2019-02-22 23:49:42 -0700

    ...

--- web/tnbUserInsert.php
+++ web/tnbUserInsert.php
@@ -1,25 +1,26 @@
 <?php
-    require "dbConnect.php";
-    $db =get_db();
+require "dbConnect.php";
+$db = get_db();
 
-    //Shared credit for the login systems belongs to Sebastian Calle,
-    //Sheldon Smith, and Stefano Nicotra. We (and by "we" I
-    //mean I feel like it was mostly them because they are such
-    //competent programers) wrote most of it in a team activty.
-    //It fit so well into my project that all I did was retyped 
-    //it out so that I understood and added a few functionality
-    // points that I wanted. I have changed very little of this
-    // code. So, special thanks to them. Used with permission.
+//Shared credit for the login systems belongs to Sebastian Calle,
+//Sheldon Smith, and Stefano Nicotra. We (and by "we" I
+//mean I feel like it was mostly them because they are such
+//competent programers) wrote most of it in a team activty.
+//It fit so well into my project that all I did was retyped 
+//it out so that I understood and added a few functionality
+// points that I wanted. I have changed very little of this
+// code. So, special thanks to them. Used with permission.
 
-    $userName = $_POSt["shipper_name"];
-    $password = $_POST["shipper_password"];
-    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+$userName = $_POSt["shipper_name"];
+$password = $_POST["shipper_password"];
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-    $statement = $db->prepare("INSERT INTO shipper (shipper_name, shipper_password_hash) 
-                                VALUES (:cleanUsername, :cleanPasswordHash)");
-    $statement->bindValue("cleanUsername", $userName, PDO::PARAM_STR);
-    $statement->bindValue("cleanPasswordHash", $passwordHash. PDO::PARAM_STR);
-    $statement->execute();
-    
-    header("Location: tnbSignIn.php");
-    exit;
+$statement = $db->prepare("INSERT INTO shipper (shipper_name, shipper_password_hash)
+    VALUES (:cleanUsername, :cleanPasswordHash)");
+$statement->bindValue(":cleanUsername", $userName, PDO::PARAM_STR);
+$statement->bindValue(":cleanPasswordHash", $passwordHash, PDO::PARAM_STR);
+$statement->execute();
+
+header("Location: tnbSignIn.php");
+exit;
+
--- web/tnbUserInsert1.php
+++ web/tnbUserInsert1.php
@@ -7,11 +7,11 @@
     $password = $_POST["shipper_password"];
     $passwordHash = password_hash($password, PASSWORD_BCRYPT);
     
-    $statement = $db->prepare("INSERT INTO shipper (shipper_name, shipper_password_hash)
+   $statement = $db->prepare("INSERT INTO shipper (shipper_name, shipper_password_hash)
                                VALUES (:cleanUsername, :cleanPasswordHash)");
     $statement->bindValue(":cleanUsername", $userName, PDO::PARAM_STR);
     $statement->bindValue(":cleanPasswordHash", $passwordHash, PDO::PARAM_STR);
-    $statement->execute();
+    $statement->execute();  
 
     header("Location: signIn.php");
     exit;
