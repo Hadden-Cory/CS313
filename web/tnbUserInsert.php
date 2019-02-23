0b05cbe145d3887ded0d5853c13c1b6beeb97740
Mona Lisa <had17007@byui.edu>
2019-02-22 23:51:09 -0700

    ...

--- web/tnbUserInsert.php
+++ web/tnbUserInsert.php
@@ -11,15 +11,15 @@ $db = get_db();
 // points that I wanted. I have changed very little of this
 // code. So, special thanks to them. Used with permission.
 
-$userName = $_POSt["shipper_name"];
+$userName = $_POST["shipper_name"];
 $password = $_POST["shipper_password"];
 $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
 $statement = $db->prepare("INSERT INTO shipper (shipper_name, shipper_password_hash)
-    VALUES (:cleanUsername, :cleanPasswordHash)");
+                           VALUES (:cleanUsername, :cleanPasswordHash)");
 $statement->bindValue(":cleanUsername", $userName, PDO::PARAM_STR);
 $statement->bindValue(":cleanPasswordHash", $passwordHash, PDO::PARAM_STR);
-$statement->execute();
+$statement->execute();  
 
 header("Location: tnbSignIn.php");
 exit;
