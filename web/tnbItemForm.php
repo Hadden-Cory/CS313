ca63ea4c7b08c98e79f908387fa7c52f805e36f6
Mona Lisa <had17007@byui.edu>
2019-02-23 15:24:00 -0700

    .....

--- web/tnbItemForm.php
+++ web/tnbItemForm.php
@@ -20,7 +20,7 @@ session_start();
     echo $_SESSION['pickUpState'];
     ?>
 
-    <form name="item" method="POST" action="tnbShipmentCache.php">
+    <form name="item" method="POST" action="tnbItemCache.php">
         <label for="name">What is your item?</label>
         <input type="text" name="name"><br>
         <label for="itemDescription">If applicatble, add a description.</label>
