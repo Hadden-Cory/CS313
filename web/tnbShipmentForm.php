f379338a70d9e36a565cc7de804875bb85a81041
Mona Lisa <had17007@byui.edu>
2019-02-23 18:29:22 -0700

    ...

--- web/tnbShipmentForm.php
+++ web/tnbShipmentForm.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 if ($_SESSION["verified"] == false) {
     session_destroy();
     header("Location: tnbSignIn.php");
--- web/totallyNotAScam.php
+++ web/totallyNotAScam.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <html>
 
 <head>
@@ -14,12 +17,7 @@
 
 <body>
     <?php include("header.php"); 
-    
-    if ($_SESSION["verified"] == false) {
-        session_destroy();
-        header("Location: tnbSignIn.php");
-        die();
-    }?>
+    ?>
     <Section>
         <div class="jumbotron">
             <h1>Welcome to Totally Not A Scam Wonderments For Internet</h1>
