30ce54548e79ee56e3555c1d7d52771c4ad9b532
Mona Lisa <had17007@byui.edu>
2019-02-23 18:52:54 -0700

    ....

--- web/tnbShipmentEdit.php
+++ web/tnbShipmentEdit.php
@@ -54,9 +54,15 @@ $db = get_db();
         echo '<td>' . htmlspecialchars($row['pickup_from_city']) . '</td>';
         echo '<td>' . htmlspecialchars($row['pickup_from_state']) . '</td>';
         echo '<td><form name="opt' . $rowCount . '" action="tnbDetails.php" " method="POST">
-                                                    <input type="text" class="collapse" name="shippmentId" value="' . htmlspecialchars($row['id_shipment']) . '">
+                                                    <input type="text" class="collapse" name="shippmentId" value="' 
+                                                    . htmlspecialchars($row['id_shipment']) . '">
                                                     <input type="submit" onclick="post" value="Remove Post"></form>
                                                           </td>';
+        echo '<td><form name="opt' . $rowCount . '" action="tnbDetails.php" " method="POST">
+                                                    <input type="text" class="collapse" name="shippmentId" value="' 
+                                                    . htmlspecialchars($row['id_shipment']) . '">
+                                                    <input type="submit" onclick="post" value="View Bids"></form>
+            </td>';
         echo "<td>" . htmlspecialchars($row['id_shipment']) . "</td>";
         echo '</tr>';
     }
