a66fc07593152e6eb3fd0b2cd093d94be324e84f
Mona Lisa <had17007@byui.edu>
2019-02-23 19:55:53 -0700

    ....

--- web/tnbBidInsert.php
+++ web/tnbBidInsert.php
@@ -8,7 +8,7 @@ if ($_SESSION["verified"] == false) {
     header("Location: tnbSignIn.php");
     die();
 }
-
+$shippmentId = $_POST['shippmentId'];
 $price = $_POST['price'];
 $startDate = $_POST['pickUpDate'];
 $endDate = $_POST['dropOffDate'];
@@ -16,13 +16,14 @@ $phone = $_POST['phone'];
 $spclInstructs = $_POST['spclInstructs'];
 
 $statement = $db->prepare("INSERT INTO bid (bid_price, bid_stat_date, bid_end_date,
-                                             bid_contact_number, bid_spcl_instruct)
-                           VALUES (:price, :bStart, :bEnd, :phone, :spcl)");
+                                             bid_contact_number, bid_spcl_instruct, shipment_id_shipment)
+                           VALUES (:price, :bStart, :bEnd, :phone, :spcl, shipId)");
 $statement->bindValue(":price", $price, PDO::PARAM_STR);
 $statement->bindValue(":bStart", $startDate, PDO::PARAM_STR);
 $statement->bindValue(":bEnd", $endDate, PDO::PARAM_STR);
 $statement->bindValue(":phone", $phone, PDO::PARAM_STR);
 $statement->bindValue(":spcl", $spclInstructs, PDO::PARAM_STR);
+$statement->bindValue(":shipId", $shippmentId, PDO::PARAM_INT);
 $statement->execute(); 
 
 header("Location: tbnBidSubmited.php");
