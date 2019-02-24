919d719701895d7d12942d57ecbfaa7805f72338
Mona Lisa <had17007@byui.edu>
2019-02-24 00:57:49 -0700

    ...

--- web/tnbViewBids.php
+++ web/tnbViewBids.php
@@ -27,93 +27,7 @@ $db = get_db();
     }
     ?>
 
-
-    <div id="container" class="center">
-        <div class="row h-50">
-            <div class="col-sm-2 col-lg-2">
-                <!-- We will be building this table for several lines. The first group is the headings, 
-                              followed by a large query and finished with a loop through all the query results-->
-                <table id="mainTable">
-                    <tr>
-                        <th>
-                            <h2>
-                                Logistics</h2>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>Pickup As Early As </th>
-                        <th>Pickup City</th>
-                        <th>Pickup State</th>
-                        <th>Deliver by</th>
-                        <th>Delivery City</th>
-                        <th>Delivery State</th>
-                    </tr>
-                    <?php
-
-                    $selectionId =  $_POST["shippmentId"];
-
-                    // We're building three tables by querying the database for general shipment information and the bid, inserting it into HTML tables 
-                    // Delivery Info Query
-                    foreach ($db->query("SELECT
-                                                            ship.shipment_start_date,
-                                                            ship.id_shipment,
-                                                            pfrom.pickup_from_city,
-                                                            pfrom.pickup_from_state, 
-                                                            ship.shipment_end_date,
-                                                            shipto.ship_to_city,
-                                                            shipto.ship_to_state
-                                                        from
-                                                            shipment as ship
-                                                                inner join shipper as shipr on ship.Shipper_id_shipper = shipr.id_shipper
-                                                                inner join ship_to as shipto on ship.id_shipment = shipto.shipment_id_shipment 
-                                                                inner join pickup_from as pfrom on ship.id_shipment = pfrom.shipment_id_shipment 
-                                                        where
-                                                        ship.id_shipment = '$selectionId'") as $row) {
-
-                        //Loop though each row and make us a html table!
-                        echo '<tr><td>' . htmlspecialchars($row['shipment_start_date']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['ship_to_city']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['ship_to_state']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['shipment_end_date']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['pickup_from_city']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['pickup_from_state']) . '</td>';
-                        echo '</tr>';
-                    }
-                    echo '</table>';
-                    echo '<table id=mainTable><tr><th><h2>
-                                Load</h2></th></tr><tr><th>Item</th>
-                                            <th>Description</th><th>Special Instructions</th>
-                                            <th>Weight</th><th>Width</th>
-                                            <th>Depth</th><th>Height</th></tr>';
-                    //Item Query
-                    foreach ($db->query(
-                        "SELECT
-                                            i.item_name,
-                                            i.item_description,
-                                            i.item_spcl_instructs,
-                                            i.shipment_id_shipment,
-                                            s.size_weight,
-                                            s.size_width,
-                                            s.size_depth,
-                                            s.size_height
-                                    from 
-                                     item as I
-                                        inner join size as s on i.size_id_size = s.id_size
-                                        where i.shipment_id_shipment = '$selectionId';"
-                    ) as $row) {
-                        //Loop though each row and make us a html table!
-                        echo '<tr><td>' . htmlspecialchars($row['item_name']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['item_description']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['item_spcl_instructs']) . '</td>';
-                        echo '<td>' . htmlspecialchars($row['size_weight']) . 'lb</td>';
-                        echo '<td>' . htmlspecialchars($row['size_width']) / 12 . 'ft ' . htmlspecialchars($row['size_width']) % 12 . 'in</td>';
-                        echo '<td>' . htmlspecialchars($row['size_depth']) / 12 . 'ft ' . htmlspecialchars($row['size_depth']) % 12 . 'in</td>';
-                        echo '<td>' . htmlspecialchars($row['size_height']) / 12 . 'ft ' . htmlspecialchars($row['size_height']) % 12 . 'in</td>';
-                        echo '</tr>';
-                    }
-                    ?>
-                </table>
-                <!-- Bid Table Time -->
+             <!-- Bid Table Time -->
                 <table>
                     <tr>
                         <th>Price</th>
@@ -142,6 +56,7 @@ $db = get_db();
                     }
                     ?>
                 </table>
+            
 </body>
 
 </html>
