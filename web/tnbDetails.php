71ebc7b97ed1e3c555fa93c58743f6751b5755bb
Mona Lisa <had17007@byui.edu>
2019-02-23 20:12:48 -0700

    ...

--- web/tnbDetails.php
+++ web/tnbDetails.php
@@ -77,7 +77,7 @@ $db = get_db();
                                                             echo '<td>' . htmlspecialchars($row['pickup_from_city']) . '</td>';
                                                             echo '<td>' . htmlspecialchars($row['pickup_from_state']) . '</td>';
                                                             echo '<td><form name="opt' . $rowCount . '" action="tnbBidForm.php" " method="POST">
-                                                            <input type="text" class="collaspe" name="shippmentId" value="'.$selectionId.'">
+                                                            <input type="text" class="collaspe" name="shipmentId" value="'.$selectionId.'">
                                                             <input type="submit" onclick="post" value="Make An Offer"></form>
                                                      </td>';
                                                             echo '</tr>';
