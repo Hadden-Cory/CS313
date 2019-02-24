3a92276686deb91e48b28a558165adaede54f80d
Mona Lisa <had17007@byui.edu>
2019-02-23 18:21:12 -0700

    ...

--- web/tnbVerifyUser.php
+++ web/tnbVerifyUser.php
@@ -1,13 +1,7 @@
 <?php
     session_start();
     require "dbConnect.php";
-    
-    if ($_SESSION["verified"] == false) {
-        session_destroy();
-        header("Location: tnbSignIn.php");
-        die();
-    }
-    
+  
     //Shared credit for the login systems belongs to Sebastian Calle,
     //Sheldon Smith, and Stefano Nicotra. We (and by "we" I
     //mean I feel like it was mostly them because they are such
