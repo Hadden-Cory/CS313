865445d8c018182351289bd49b9c82b8bc042ca4
Mona Lisa <had17007@byui.edu>
2019-02-23 23:58:04 -0700

    ...

--- web/thereNBack.php
+++ web/thereNBack.php
@@ -1,5 +1,5 @@
 <?php
-//Start Session, connect to db, live happily ever after.
+ //Start Session, connect to db, live happily ever after.
 session_start();
 require("dbConnect.php");
 $db = get_db();
@@ -19,16 +19,30 @@ $db = get_db();
 
 <body>
     <?php include("header.php");
-        if ($_SESSION["verified"] == false) {
-            session_destroy();
-            header("Location: tnbSignIn.php");
-            die();
-        }?>
+    if ($_SESSION["verified"] == false) {
+        session_destroy();
+        header("Location: tnbSignIn.php");
+        die();
+    } ?>
 
     <div id="container" class="center">
         <div class="row h-50">
             <div class="col-sm-2 col-lg-2">
-                <h1>Welcome <?php echo htmlspecialchars($shipper_name); ?></h1>
+            </div>
+            <div class="col-sm-8 col-lg-8">
+                <h1>Welcome
+                    <?php echo htmlspecialchars($_SESSION["username"]); ?>
+                </h1>
+                <h2>
+                 Let's Move A Mountain
+                </h2>
+            </div>
+            <div class="col-sm-2 col-lg-2">
+            </div>
+        </div>
+        <div class="row h-50">
+            <div class="col-sm-2 col-lg-2">
+
                 <form name="form" action="tnbLogout.php" method="post">
                     <input type="submit" class="btn btn-light w-100" value="Logout">
                 </form>
@@ -50,8 +64,8 @@ $db = get_db();
                     </tr>
 
                     <?php 
-                    $rowCount = 0;  
-                    
+                    $rowCount = 0;
+
                     //We're building a table by querying the database for general shipment information and inserting it into a HTML table 
                     //Query
                     foreach ($db->query('SELECT
@@ -69,28 +83,27 @@ $db = get_db();
                                                 inner join pickup_from as pfrom on ship.id_shipment = pfrom.shipment_id_shipment 
                                             order by
                                                 ship.id_shipment DESC,
-                                                ship_to_city DESC;') as $row) 
-                                                {
-                                                    //Loop though each row and make us a html table!
-                                                    $rowCount++;
-                                                    
-                                                    echo '<tr><td>' . htmlspecialchars($row['shipment_start_date']) . '</td>';
-                                                    echo '<td>' . htmlspecialchars($row['ship_to_city']) . '</td>';
-                                                    echo '<td>' . htmlspecialchars($row['ship_to_state']) . '</td>';
-                                                    echo '<td>' . htmlspecialchars($row['shipment_end_date']) . '</td>';
-                                                    echo '<td>' . htmlspecialchars($row['pickup_from_city']) . '</td>';
-                                                    echo '<td>' . htmlspecialchars($row['pickup_from_state']) . '</td>';
-                                                    echo '<td><form name="opt' . $rowCount . '" action="tnbDetails.php" " method="POST">
-                                                                 <input type="text" class="collapse" name="shippmentId" value="'.htmlspecialchars($row['id_shipment']).'">
+                                                ship_to_city DESC;') as $row) {
+                        //Loop though each row and make us a html table!
+                        $rowCount++;
+
+                        echo '<tr><td>' . htmlspecialchars($row['shipment_start_date']) . '</td>';
+                        echo '<td>' . htmlspecialchars($row['ship_to_city']) . '</td>';
+                        echo '<td>' . htmlspecialchars($row['ship_to_state']) . '</td>';
+                        echo '<td>' . htmlspecialchars($row['shipment_end_date']) . '</td>';
+                        echo '<td>' . htmlspecialchars($row['pickup_from_city']) . '</td>';
+                        echo '<td>' . htmlspecialchars($row['pickup_from_state']) . '</td>';
+                        echo '<td><form name="opt' . $rowCount . '" action="tnbDetails.php" " method="POST">
+                                                                 <input type="text" class="collapse" name="shippmentId" value="' . htmlspecialchars($row['id_shipment']) . '">
                                                                  <input type="submit" class="btn btn-light" onclick="post" value="Load Info"></form>
                                                           </td>';
-                                                    echo '</tr>';
+                        echo '</tr>';
                     }
                     echo '</table>'; //Make the table stop 
- ?>
+                    ?>
             </div>
             <div class="col-sm-2 col-lg-2">
             </div>
         </div>
     </div>
-    <section>
+    <section> 
